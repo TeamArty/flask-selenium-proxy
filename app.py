5351137740d2eeca81efa0f54ed6e6a2c34a8eca
TeamArty <luka.tasev.mkd@gmail.com>
2023-11-19 15:07:49 +0100

    asx

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import flask
 from flask import Flask, request
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import undetected_chromedriver as uc
 
 # > Define Flask App
 app     = Flask(__name__)
@@ -11,12 +10,10 @@ options.add_argument("--no-sandbox")
 options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36')
-#driver  = webdriver.Chrome(options=options)
+print(options.binary_location)
+driver  = webdriver.Chrome(options=options)
 print(options.binary_location)
 
-options = uc.ChromeOptions()
-options.binary_location = options.binary_location
-test = uc.Chrome(options=options)
 
 # > GET /
 @app.get("/")
@@ -27,14 +24,3 @@ def get():
     global driver
     driver.get(request.args.get("url"))
     return flask.Response(driver.page_source, mimetype="text/plain")
-
-
-# > GET /test
-@app.get("/test")
-def get():
-    if request.args.get("url", None) == None:
-        return flask.Response("No URL Specified", mimetype="text/plain")
-    
-    global test
-    test.get(request.args.get("url"))
-    return flask.Response(test.page_source, mimetype="text/plain")
