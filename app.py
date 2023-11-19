ce8909d4922a69551e6f06b4b89a773f2191c4f6
TeamArty <luka.tasev.mkd@gmail.com>
2023-11-19 13:33:58 +0100

    test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 from flask import Flask, request
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import undetected_chromedriver as uc
 
 # > Define Flask App
 app     = Flask(__name__)
@@ -9,8 +10,12 @@ options = webdriver.ChromeOptions()
 options.add_argument("--no-sandbox")
 options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299')
+options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36')
 driver  = webdriver.Chrome(options=options)
+
+options = uc.ChromeOptions()
+options.binary_location = "/opt/render/project/.render/chrome"
+test = uc.Chrome(options=options)
 
 # > GET /
 @app.get("/")
@@ -21,23 +26,3 @@ def get():
     global driver
     driver.get(request.args.get("url"))
     return flask.Response(driver.page_source, mimetype="text/plain")
-
-# > GET /cai
-@app.get("/cai")
-def cai():
-    if request.args.get("id", None) == None:
-        return flask.Response("No id Specified", mimetype="text/plain")
-    id = request.args.get("id", None)
-    
-    global driver
-    driver.get("https://beta.character.ai/chat?char=" + id)
-    return driver.page_source
-    #javascript = '''var xhr = new XMLHttpRequest();
-    #xhr.open('POST', 'https://beta.character.ai/chat/character/info/', false);
-    #xhr.setRequestHeader('Content-type', 'application/json');
-    #xhr.setRequestHeader('Authorization', 'Token be4699378133b71855e683d63eba698f0f0d870c');
-    #xhr.send('{"external_id":"''' + id + '''"}');
-    #return xhr.response;
-    #'''
-    #result = driver.execute_script(javascript)
-    #return result
--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ else
   echo "...Using Chrome from cache"
 fi
 
+ls /opt/render/project/.render/chrome
 # > IMPORTANT - set this as the build command:
 # export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"; ./build.sh;
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
